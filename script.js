f2d377f51091e1c8317cef5e50bbee1483e815a5
roukell <62136980+roukell@users.noreply.github.com>
2020-06-24 22:49:28 +1000

    update: when enter city, city will apear on list and stored in localStorage

--- script.js
+++ script.js
@@ -1,9 +1,51 @@
-$(".fa-search").on("click", function () {
+let cityList = document.querySelector(".city-section");
+
+
+// get input from local storage
+let storedCity = JSON.parse(localStorage.getItem("storedCity"));
+
+if (storedCity !== null) {
+    cityArray = storedCity;
+} else {
+    cityArray = new Array();
+    cityArray[0] = "Sydney";
+}
+
+renderCities();
+
+// console.log(cityArray);
+// console.log(cityArray.length);
+
+function renderCities() {
+    for (i = 0; i < cityArray.length; i++) {
+    console.log(cityArray[i]);
+    let cityli = document.createElement("li");
+    cityli.innerHTML = cityArray[i];
+    cityList.appendChild(cityli);
+}
+}
+    
+
+// when clicked on search icon
+// new city will be saved in localStorage
+$(".fa-search").on("click", function (event) {
+    event.preventDefault();
     let cityEntered = $("input.input-section").val();
-    const cityDiv = $("<div>");
-    if (cityEntered !== "") {
-            cityDiv.attr("class", "cityDiv");
-            cityDiv.text(cityEntered);
-            $(".city-section").prepend(cityDiv);
+
+    if (cityEntered === "") {
+        return;
+    }
+
+    for (i = 0; i < cityArray.length; i++) {
+        if (cityEntered === cityArray[i]) {
+            return;
         }
+    }
+    // push new city in cityArray
+    // and store in localStorage
+    cityArray.push(cityEntered);
+    localStorage.setItem("storedCity", JSON.stringify(cityArray));
+    
+    renderCities();
 })
+

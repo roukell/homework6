cf3fcdafa62320d9c7cf2d614a112be2df188750
roukell <62136980+roukell@users.noreply.github.com>
2020-06-27 15:59:10 +1000

    add ajax call

--- script.js
+++ script.js
@@ -45,11 +45,32 @@ $(".fa-search").on("click", function (event) {
     renderCities();
 })
 
-// call API for 5 days forecast
-const keyAPI = "be69296eb40c19cd4eb4d12be03cdf06";
-const queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + cityArray[cityArray.length - 1] + "&appid=" + keyAPI;
+// call API for the 5 days forecast of cityEntered
+const keyAPI = "4acbae4fdb0b64a992f7caade418dc6d";
+const queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityArray[cityArray.length - 1] + "&appid=" + keyAPI;
+const cityListJson = 
 
 console.log(queryURL);
 
+// create a function: to display current forecast of cityEntered in .card-section
+function displayCurrentForecast() {
+    // create AJAX call
+    $.ajax({
+        url:queryURL,
+        method: "GET"
+    }).then(function(response){
 
+        // create a div to hold forecast information
+        // const forecastInfo = $("<div>");
+        // forecastInfo.addClass("forcastInfo");
+        // $(".card-section").append(forecastInfo);
+        console.log(cityArray[cityArray.length - 1]);
+        console.log(response.list[0].main.temp);
+        console.log(response.list[0].main.humidity);
+        console.log(response.list[0].wind.speed);
+    })
+
+}
+
+displayCurrentForecast();
 
